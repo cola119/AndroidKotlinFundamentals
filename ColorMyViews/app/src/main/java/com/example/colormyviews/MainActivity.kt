c6b05b99f55856e3c89920c85033c8a1e64299ab
cola119 <k.cola119@ezweb.ne.jp>
2020-04-19 14:04:38 +0900

    done

--- ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,8 +22,12 @@ class MainActivity : AppCompatActivity() {
         val box4 = findViewById<TextView>(R.id.box_four_text)
         val box5 = findViewById<TextView>(R.id.box_five_text)
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
+        val redButton = findViewById<TextView>(R.id.red_button)
+        val greenButton = findViewById<TextView>(R.id.green_button)
+        val yellowButton = findViewById<TextView>(R.id.yellow_button)
 
-        val clickableViews: List<View> = listOf(box1, box2, box3, box4, box5, rootConstraintLayout)
+        val clickableViews: List<View> =
+            listOf(box1, box2, box3, box4, box5, rootConstraintLayout, redButton, greenButton, yellowButton)
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
         }
@@ -35,6 +40,9 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
